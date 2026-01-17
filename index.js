dfda8f81e27b6ea1315bc97967f67177c67cab58
Your Full Name <rhodaandrian@gmail.com>
2026-01-17 16:42:45 +0300

    Lab-password

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  return (passwordAttempt) => {
+    if (attemptCount >= 3) {
+      return "Account locked due to too many failed login attempts";
+    }
 
+    attemptCount++;
+
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+}
 
 module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
+  createLoginTracker
 };
